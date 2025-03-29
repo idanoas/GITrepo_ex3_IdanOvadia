4498f5b13ba6f1b6067b4903cf3349fa0a874cff
idanoas <idanoas@gmail.com>
2025-03-29 03:51:51 +0200

    added to files

---
+++ yourname.java
@@ -0,0 +1,14 @@
+public class IdanOvadia {
+	public static void main (String [] args)
+	{
+		for (int i=1; i<=10; i++)
+		{
+			System.out.println("Idan Ovadia");
+			
+		}
+		System.out.println("Idan Ovadia is liking it");
+
+	}
+
+
+}
