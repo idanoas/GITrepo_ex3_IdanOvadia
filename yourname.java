036b885fb8dd22fd6d4411c82539d7c2e7580dd7
idanoas <idanoas@gmail.com>
2025-03-29 09:06:54 +0200

    modified two files

--- yourname.java
+++ yourname.java
@@ -1,7 +1,7 @@
-public class IdanOvadia {
+public class yourname {
 	public static void main (String [] args)
 	{
-		for (int i=1; i<=10; i++)
+		for (int i=1; i<=7; i++)
 		{
 			System.out.println("Idan Ovadia");
 			
